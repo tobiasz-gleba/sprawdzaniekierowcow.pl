9b82fca008ae8ea5fb0c852e1bcba73a83b6b38f
Tobiasz Gleba <tobiasz.gleba24@gmail.com>
2025-10-16 14:35:59 +0200

    feat: add daisy UI

--- app/src/routes/login/+page.svelte
+++ app/src/routes/login/+page.svelte
@@ -5,29 +5,65 @@
 	let { form }: { form: ActionData } = $props();
 </script>
 
-<h1>Login/Register</h1>
-<form method="post" action="?/login" use:enhance>
-	<label>
-		Email
-		<input
-			type="email"
-			name="email"
-			class="mt-1 px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-		/>
-	</label>
-	<label>
-		Password
-		<input
-			type="password"
-			name="password"
-			class="mt-1 px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-		/>
-	</label>
-	<button class="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition"
-		>Login</button>
-	<button
-		formaction="?/register"
-		class="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition"
-		>Register</button>
-</form>
-<p style='color: red'>{form?.message ?? ''}</p>
+<div class="hero min-h-screen bg-base-200">
+	<div class="hero-content flex-col lg:flex-row-reverse">
+		<div class="text-center lg:text-left">
+			<h1 class="text-5xl font-bold">Login or Register</h1>
+			<p class="py-6">
+				Enter your credentials to access your account or create a new one.
+			</p>
+		</div>
+		<div class="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+			<form class="card-body" method="post" action="?/login" use:enhance>
+				{#if form?.message}
+					<div role="alert" class="alert alert-error">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="h-6 w-6 shrink-0 stroke-current"
+							fill="none"
+							viewBox="0 0 24 24">
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								stroke-width="2"
+								d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+						</svg>
+						<span>{form.message}</span>
+					</div>
+				{/if}
+				<div class="form-control">
+					<label class="label" for="email">
+						<span class="label-text">Email</span>
+					</label>
+					<input
+						id="email"
+						type="email"
+						name="email"
+						placeholder="email@example.com"
+						class="input input-bordered"
+						required
+					/>
+				</div>
+				<div class="form-control">
+					<label class="label" for="password">
+						<span class="label-text">Password</span>
+					</label>
+					<input
+						id="password"
+						type="password"
+						name="password"
+						placeholder="Enter your password"
+						class="input input-bordered"
+						required
+					/>
+				</div>
+				<div class="form-control mt-6 gap-2">
+					<button class="btn btn-primary">Login</button>
+					<button formaction="?/register" class="btn btn-secondary">
+						Register
+					</button>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
